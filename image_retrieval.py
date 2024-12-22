c3b4b1f9c8621ca667eb499b50e23afcc2061fee
Wang Xigui <xigui.wang@qq.com>
2024-12-22 07:29:03 -0800

    Enable on Windows

--- image_retrieval.py
+++ image_retrieval.py
@@ -14,34 +14,42 @@ from utils import display_images_in_batch, filter_match_image, generate_template
 from clip_embedding import CLIP_Embedding
 from blip import LVM_model
 
+import multiprocessing
+
 import argparse
 
-# Create the parser
-parser = argparse.ArgumentParser(description="Process embedding input.")
-parser.add_argument(
-    "--embedding",
-    action="store_true",
-    default=False,   # Default value set to False
-    help="Set embedding to True (default: False)"
-)
-# Parse the arguments
-args = parser.parse_args()
+def load_clip_model():    
+    model_name = "openai/clip-vit-base-patch32"
+    device = "xpu"
+    try:
+        embedding_model = CLIP_Embedding(model_name, device)
+        print("Model CLIP loaded successfully!")
+        return embedding_model
+    except Exception as e:
+        print(f"Error loading model: {e}")
 
-# Initialize Redis connection
-r = redis.Redis(host='localhost', port=6379, db = 0) #decode_responses=True)
+def load_lvm_model():
+    lvm_device = "xpu"
+    lvm_model_name = "Salesforce/blip2-flan-t5-xl"
+    try:
+        #lvm_model_name = "/home/xwang/.cache/huggingface/hub/models--Salesforce--blip2-flan-t5-xl/snapshots/2839125572785ff89d2438c8bf1550a98c7fcfcd"
+        multiModel = LVM_model(lvm_model_name, lvm_device)
+        print("Model LVM loaded successfully!")
+        return multiModel
+    except Exception as e:
+        print(f"Error loading {lvm_model_name} model: {e}")
 
-Index_name = "myIndex"
-if (not is_index_existed(Index_name)):
-    create_index(Index_name)
+def connect_redis():
+    try:
+        # Initialize Redis connection
+        redis_db = redis.Redis(host='localhost', port=6379, db = 0) #decode_responses=True)
+        Index_name = "myIndex"
+        if (not is_index_existed(redis_db, Index_name)):
+            create_index(redis_db, Index_name)
+    except Exception as e:
+        print(f"Error connect_redis: {e}")
 
-model_name = "openai/clip-vit-base-patch32"
-device = "xpu"
-embedding_model = CLIP_Embedding(model_name, device)
-
-#lvm_model_name = "Salesforce/blip2-flan-t5-xl"
-lvm_model_name = "/home/xwang/.cache/huggingface/hub/models--Salesforce--blip2-flan-t5-xl/snapshots/2839125572785ff89d2438c8bf1550a98c7fcfcd"
-lvm_device = "xpu"
-multiModel = LVM_model(lvm_model_name, lvm_device)
+    return redis_db
 
 # Function to preprocess images
 def preprocess_images(image_paths):
@@ -50,7 +58,7 @@ def preprocess_images(image_paths):
     return images, full_image_paths
 
 # Function to embed and store images in Redis
-def embed_and_store_images(image_paths, batch_size=32):
+def embed_and_store_images(embedding_model, redis_db, image_paths, batch_size=32):
     # Step 1: Process images in batches
     for i in tqdm(range(0, len(image_paths), batch_size), desc="Processing batches"):
         batch_paths = image_paths[i:i+batch_size]
@@ -71,79 +79,110 @@ def embed_and_store_images(image_paths, batch_size=32):
             date = extract_date_from_jpeg(images[idx])
 
             # Store the image vector in Redis
-            r.hset(image_id, mapping={"vector": image_vector, "id": image_id, "path": image_path[idx], "date": date})
+            redis_db.hset(image_id, mapping={"vector": image_vector, "id": image_id, "path": image_path[idx], "date": date})
 
         print(f"Batch {i//batch_size + 1} processed and stored in Redis.")
 
 # Function to retrieve similar images based on a query
-def search_similar_images(query_image_path, top_k=5):
+def search_similar_images(embedding_model, db, query_image_path, top_k=5):
     # Step 1: Process the query image
     query_image = Image.open(query_image_path).convert("RGB")
     # Step 2: Get image embedding for the query image
     query_embedding = embedding_model.get_image_embeddings(query_image) 
 
-    search_images_path = search_images_by_embedding(query_embedding, top_k)
-    return search_images_path 
+    search_images_path = search_images_by_embedding(db, query_embedding, top_k)
+    return search_images_path
 
 # Function to retrieve similar images based on a query
-def search_images_by_embedding(query_embedding, top_k):
+def search_images_by_embedding(redis_db, query_embedding, top_k):
     query_vector = np.array(query_embedding.cpu(), dtype=np.float32).tobytes()  # Convert to bytes for Redis
     query_str = f"*=>[KNN {top_k} @vector $query_vector AS score]"  # Top k nearest neighbors
 
     # Perform the search (Redis will return the closest matches)
-    search_results = r.ft("myIndex").search(
+    search_results = redis_db.ft("myIndex").search(
         query=Query(query_str).
-        sort_by("score", asc=False).
+        sort_by("score", asc=True).
         #return_fields("id", "path", "vector").paging(0, top_k).dialect(2),
         return_fields("id", "path", "vector", "score").paging(0, top_k).dialect(2),
         query_params={"query_vector": query_vector},
     )
 
     search_images_path = [result.path for result in search_results.docs]
-    #score = [result.score for result in search_results.docs]
+    score = [result.score for result in search_results.docs]
     #print(score)
     return search_images_path 
 
-# Example usage
-image_folder = "/mnt/Photo"  # Folder containing images
-image_paths = [os.path.join(image_folder, fname) for fname in os.listdir(image_folder) if fname.endswith(".jpg")]
+def main():
+    embedding_model = load_clip_model()
+    multiModel = load_lvm_model()
+    redis_db = connect_redis()
 
-# Process the images and store them in Redis
-if (args.embedding):
-    print("Start ingest images into Database. ")
-    print(f"Process images in {image_folder} Waiting for minutes ....")
-    embed_and_store_images(image_paths, batch_size=128)
+    # Example usage
+    #image_folder = "E:/Photo"  # Folder containing images
+    #image_paths = [os.path.join(image_folder, fname) for fname in os.listdir(image_folder) if fname.endswith(".jpg")]
 
-# Example: Querying for a similar image
-query_image_path = "/home/xwang/Downloads/image/0201_18.jpg"
-#search_imgae_paths = search_similar_images(query_image_path, top_k=5)
-#query_image = Image.open(query_image_path).convert("RGB")
-#query_embedding = embedding_model.get_image_embeddings(query_image)
-#search_imgae_paths = search_images_by_embedding(query_embedding, top_k=5)
+    # Process the images and store them in Redis
+    if (args.embedding):
+        if (args.folder is not None):
+            image_folder = args.folder
+            image_paths = [os.path.join(image_folder, fname) for fname in os.listdir(image_folder) if fname.lower().endswith(".jpg")]
+            print("Start ingest images into Database. ")
+            print(f"Process images in {image_folder} Waiting for minutes ....")
+            embed_and_store_images(embedding_model, redis_db, image_paths, batch_size=32)
 
-query = ["A photo of Family"] # Input Query
-print("Family photo")
-text_embeddings = embedding_model.embed_query(query) 
-search_imgae_paths = search_images_by_embedding(text_embeddings, top_k=4)
-display_images_in_batch(search_imgae_paths)
+    query = ["A photo of Family"] # Input Query
+    print("Family photo")
+    text_embeddings = embedding_model.embed_query(query) 
+    search_imgae_paths = search_images_by_embedding(redis_db, text_embeddings, top_k=4)
+    display_images_in_batch(search_imgae_paths)
 
-top_k = 60
-while True:
-    user_input = input("Enter something (type 'exit' to quit): ").strip()  # Strip removes extra spaces
-    if user_input.lower() == "exit":  # Check if the input is 'exit', case insensitive
-        print("Exiting the loop. Goodbye!")
-        break
-    else:
-        print(f"You entered: {user_input}")
-        if (len(user_input) > 0):
-            text_embeddings = embedding_model.embed_query(user_input) 
-            search_imgae_paths = search_images_by_embedding(text_embeddings, top_k)
-            print("search images:", len(search_imgae_paths))
-            # display_images_in_batch(search_imgae_paths)
-            # Display the results
-            query_string = generate_template(user_input)
-            questions = [query_string] * len(search_imgae_paths) 
-            answers = multiModel.get_image_query_answer(search_imgae_paths, questions)
-            match_image = filter_match_image(search_imgae_paths, answers)
-            print(f"accuracy iamges:", len(match_image))
-            display_images_in_batch(match_image)
+    top_k = 60
+    while True:
+        user_input = input("Enter something (type 'exit' to quit): ").strip()  # Strip removes extra spaces
+        if user_input.lower() == "exit":  # Check if the input is 'exit', case insensitive
+            print("Exiting the loop. Goodbye!")
+            break
+        else:
+            print(f"You entered: {user_input}")
+            if (len(user_input) > 0):
+                text_embeddings = embedding_model.embed_query(user_input) 
+                search_imgae_paths = search_images_by_embedding(redis_db, text_embeddings, top_k)
+                print("search images:", len(search_imgae_paths))
+                # display_images_in_batch(search_imgae_paths)
+                # Display the results
+                query_string = generate_template(user_input)
+                questions = [query_string] * len(search_imgae_paths) 
+                answers = multiModel.get_image_query_answer(search_imgae_paths, questions)
+                match_image = filter_match_image(search_imgae_paths, answers)
+                print(f"accuracy iamges:", len(match_image))
+                display_images_in_batch(match_image)
+
+if __name__ == '__main__':
+    # Explicitly set the start method for Windows
+    multiprocessing.set_start_method('spawn')
+
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Process embedding input.")
+    parser.add_argument(
+        "--embedding",
+        action="store_true",
+        default=False,   # Default value set to False
+        help="Set embedding to True (default: False)"
+    )
+
+    parser.add_argument(
+        "--folder",
+        type=str,
+        default="image_path",   # Default value set to False
+        help="Set embedding to True (default: False)"
+    )
+    # Parse the arguments
+    args = parser.parse_args()
+    main()
+
+    # Example: Querying for a similar image
+    #query_image_path = "/home/xwang/Downloads/image/0201_18.jpg"
+    #search_imgae_paths = search_similar_images(query_image_path, top_k=5)
+    #query_image = Image.open(query_image_path).convert("RGB")
+    #query_embedding = embedding_model.get_image_embeddings(query_image)
+    #search_imgae_paths = search_images_by_embedding(query_embedding, top_k=5)
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog, Label, Entry
 
+from image_retrieval import load_clip_model, load_lvm_model, connect_redis
+from image_retrieval import search_images_by_embedding, search_similar_images
+from utils import display_images_in_batch, filter_match_image, generate_template
+
+import threading
+
+user_input = "a photo of Family"
+input_image = "test.image"
+
 def open_file_dialog():
+    global input_image
+    global multiModel
+    global embedding_model
+    global redis_db
+
+    global entry, label_text, button_text, button_file, label_file
     # Open file dialog to select an image
     file_path = filedialog.askopenfilename(
         title="Select an Image",
@@ -10,39 +25,83 @@ def open_file_dialog():
     
     if file_path:
         # Update the label with the selected file path
+        top_k = 30
+        input_image = file_path
         label_file.config(text=f"Selected: {file_path}")
+        search_imgae_paths = search_similar_images(embedding_model, redis_db, input_image, top_k)
+        display_images_in_batch(search_imgae_paths)
     else:
         label_file.config(text="No file selected.")
 
 def get_text_input():
+    global user_input
+    global multiModel
+    global embedding_model
+    global redis_db
+    global entry, label_text, button_text, button_file
+
     # Get the input text from the entry box
     user_input = entry.get()
     label_text.config(text=f"Input: {user_input}")
+    text_embeddings = embedding_model.embed_query(user_input) 
+    top_k = 60
+    search_imgae_paths = search_images_by_embedding(redis_db, text_embeddings, top_k)
 
-# Create the main Tkinter window
-root = tk.Tk()
-root.title("Image Selector with Text Input")
-root.geometry("500x300")
+    query_string = generate_template(user_input)
+    questions = [query_string] * len(search_imgae_paths) 
+    answers = multiModel.get_image_query_answer(search_imgae_paths, questions)
+    match_image = filter_match_image(search_imgae_paths, answers)
+    print(f"accuracy iamges:", len(match_image))
+    display_images_in_batch(match_image)
 
-# Add an entry box for text input
-entry = Entry(root, width=40)
-entry.pack(pady=10)
+def load_models():
+    global embedding_model, multiModel, redis_db
+    embedding_model = load_clip_model()  # Load huggingface transformers models
+    multiModel = load_lvm_model()        # Load huggingface transformers models
+    redis_db = connect_redis()           # Connect to Redis
+    print("Models loaded successfully")
 
-# Add a button to submit the text input
-button_text = tk.Button(root, text="Submit Text", command=get_text_input)
-button_text.pack(pady=5)
+entry = None
+label_text = None
+button_text = None
+button_file = None
+label_file = None
 
-# Add a label to display the text input
-label_text = Label(root, text="Enter text above and click submit.")
-label_text.pack(pady=10)
+def create_gui():
+    global entry, label_text, button_text, button_file, label_file
 
-# Add a button to open the file dialog
-button_file = tk.Button(root, text="Select Image", command=open_file_dialog)
-button_file.pack(pady=10)
+    # Create the main Tkinter window
+    root = tk.Tk()
+    root.title("Image Selector with Text Input")
+    root.geometry("1200x768+100+200")
 
-# Add a label to display the selected file path
-label_file = Label(root, text="No file selected.")
-label_file.pack(pady=10)
+    # Add an entry box for text input
+    entry = Entry(root, width=40, font=("Arial", 14))
+    entry.pack(pady=10)
 
-# Start the Tkinter event loop
-root.mainloop()
+    # Add a button to submit the text input
+    button_text = tk.Button(root, text="Submit Text", font=("Arial", 14), command=get_text_input)
+    button_text.pack(pady=5)
+
+    # Add a label to display the text input
+    label_text = Label(root, text="Enter text above and click submit.", font=("Arial", 14))
+    label_text.pack(pady=10)
+
+    # Add a button to open the file dialog
+    button_file = tk.Button(root, text="Select Image", command=open_file_dialog, font=("Arial", 14))
+    button_file.pack(pady=10)
+
+    # Add a label to display the selected file path
+    label_file = Label(root, text="No file selected.", font=("Arial", 14))
+    label_file.pack(pady=10)
+
+    # Start the Tkinter event loop
+    root.mainloop()
+
+def main():
+    # Create and start a separate thread to load models
+    load_models()
+    create_gui()
+
+if __name__ == '__main__':
+    main()
